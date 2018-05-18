a79e127005f1323bc4c2ebdc33396ae5c65338dd
harbak62 <harbak62@hotmail.com>
2018-05-18 11:50:11 -0700

    Updated to lecture 73

--- Project12/BreakOut_Room0d1/Source/BreakOut_Room0d1/OpenDoor.cpp
+++ Project12/BreakOut_Room0d1/Source/BreakOut_Room0d1/OpenDoor.cpp
@@ -1,8 +1,13 @@
 // This stuff belongs to Andy 
+#pragma once
+
 
 #include "BreakOut_Room0d1.h"
 #include "OpenDoor.h"
+#include "Engine.h"
+#include "Engine/TriggerVolume.h"
 
+void OpenDoor();
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -22,18 +27,9 @@ void UOpenDoor::BeginPlay()
 
 	// ...
 
-	FString OwnerName = GetOwner()->GetName();
-	FString OwnerRotation = FString::SanitizeFloat(GetOwner()->GetTransform().GetRotation().GetAxisZ().Z);
+	OpenDoor();
 
-	UE_LOG(LogTemp, Warning, TEXT("%s Rotation is %s!!\n"), *OwnerName, *OwnerRotation)
-
-	FRotator OwnerRotate = GetOwner()->GetActorRotation();
-	OwnerRotate.Yaw += 40;
-	GetOwner()->SetActorRotation(OwnerRotate);
-
-	OwnerRotation = GetOwner()->GetTransform().GetRotation().GetRotationAxis().UpVector.ToString();
-
-	UE_LOG(LogTemp, Warning, TEXT("%s Rotation is %s!!\n"), *OwnerName, *OwnerRotation)
+	
 
 }
 
@@ -44,5 +40,25 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+}
+
+void OpenDoor()
+{
+	FString OwnerName = GetOwner()->GetName();
+	FString OwnerRotation = FString::SanitizeFloat(GetOwner()->GetTransform().GetRotation().GetAxisZ().Z);
+
+	UE_LOG(LogTemp, Warning, TEXT("%s Rotation is %s!!\n"), *OwnerName, *OwnerRotation)
+
+		FRotator OwnerRotate = GetOwner()->GetActorRotation();
+	if (OwnerName == "SM_Door_5")
+	{
+		OwnerRotate.Yaw += 40;
+		GetOwner()->SetActorRotation(OwnerRotate);
+	}
+
+	OwnerRotation = GetOwner()->GetTransform().GetRotation().GetRotationAxis().UpVector.ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s Rotation is %s!!\n"), *OwnerName, *OwnerRotation)
+
 }
 
--- Project12/BreakOut_Room0d1/Source/BreakOut_Room0d1/OpenDoor.h
+++ Project12/BreakOut_Room0d1/Source/BreakOut_Room0d1/OpenDoor.h
@@ -3,26 +3,38 @@
 #pragma once
 
 #include "Components/ActorComponent.h"
+#include "Engine.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BREAKOUT_ROOM0D1_API UOpenDoor : public UActorComponent
 {
 	GENERATED_BODY()
+
 
 public:	
 	// Sets default values for this component's properties
 	UOpenDoor();
 
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
-	
+private:
+	UPROPERTY(EditAnywhere)
+		float OpenAngle = 90.0f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+		AActor* ActorThatOpens;
+
+
 };
